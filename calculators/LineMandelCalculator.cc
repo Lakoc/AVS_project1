40f8f8aab7fc49d2865e685542f13029e2d50cfb
Lakoc <xpolok03@stud.fit.vutbr.cz>
2021-11-01 10:17:34 +0100

    Line optimized for avx512

--- calculators/LineMandelCalculator.cc
+++ calculators/LineMandelCalculator.cc
@@ -8,55 +8,69 @@
 #include <cstdlib>
 #include <cstring>
 #include "LineMandelCalculator.h"
-
+#define AVX512_IN_BYTES 64
 
 LineMandelCalculator::LineMandelCalculator(unsigned matrixBaseSize, unsigned limit) :
         BaseMandelCalculator(matrixBaseSize, limit, "LineMandelCalculator") {
-    data = (int *) (malloc(height * width * sizeof(int)));
+    data = (int *) (aligned_alloc(AVX512_IN_BYTES,height * width * sizeof(int)));
+
+    // Array of arranged Xs in range(x_start, x_start + width * dx)
+    xInitValA = static_cast<float *>(aligned_alloc(AVX512_IN_BYTES, sizeof(float) * width));
+
+    // Arrays keeping imaginary and real values of each complex number on current row
+    zRealA = static_cast<float *>(aligned_alloc(AVX512_IN_BYTES, sizeof(float) * width));
+    zImagA = static_cast<float *>(aligned_alloc(AVX512_IN_BYTES, sizeof(float) * width));
+    conditionAchievedA = static_cast<int *>(aligned_alloc(AVX512_IN_BYTES, sizeof(int) * width));
+
+    // Cast doubles to floats
+    auto xStart = static_cast<float>(x_start);
+    auto dxF = static_cast<float>(dx);
+
+    float *xInitValAP = xInitValA;
+    // Arrange Xs
+    #pragma omp simd aligned(xInitValAP:64) simdlen(64)
+    for (size_t point = 0; point < width; point++) {
+        xInitValAP[point] = xStart + static_cast<float>(point) * dxF;
+    }
 }
 
 LineMandelCalculator::~LineMandelCalculator() {
     free(data);
+    free(xInitValA);
+    free(zRealA);
+    free(zImagA);
+    free(conditionAchievedA);
     data = nullptr;
+    xInitValA = nullptr;
+    zRealA = nullptr;
+    zImagA = nullptr;
+    conditionAchievedA = nullptr;
 }
 
 
 int *LineMandelCalculator::calculateMandelbrot() {
-    int *pdata = data;
-    // Array of arranged Xs in range(x_start, x_start + width * dx)
-    alignas(128) float xInitValA[width];
+    // copy reference to use in pragma omp
+    int *dataP = data;
+    float *xInitValAP = xInitValA;
+    float *zRealAP = zRealA;
+    float *zImagAP = zImagA;
+    int *conditionAchievedAP = conditionAchievedA;
 
-    // Arrays keeping imaginary and real values of each complex number on current row
-    alignas(128) float zRealA[width];
-    alignas(128) float zImagA[width];
-
-    // Array of counts of limit achievements of each complex number on current row
-    alignas(128) int conditionAchievedA[width];
-
-    // Cast floats to doubles
-    auto xStart = static_cast<float>(x_start);
+    // Cast doubles to floats
     auto yStart = static_cast<float>(y_start);
-    auto dxF = static_cast<float>(dx);
     auto dyF = static_cast<float>(dy);
-
-    // Arrange Xs
-    #pragma omp simd aligned(xInitValA:128)
-    for (size_t point = 0; point < width; point++) {
-        xInitValA[point] = xStart + static_cast<float>(point) * dxF;
-    }
 
     // Iterate over all rows
     for (size_t row = 0; row < height; row++) {
         // Calculate imaginary value of selected row
         auto imag = yStart + static_cast<float>(row) * dyF;
 
-
         // Copy starting x's to current row, clean up limits, copy imaginary value
-        #pragma omp simd aligned(xInitValA, zImagA, zRealA, conditionAchievedA:128)
+        #pragma omp simd aligned(xInitValAP, zImagAP, zRealAP, conditionAchievedAP:64) linear(row:1)
         for (size_t point = 0; point < width; point++) {
-            zImagA[point] = imag;
-            zRealA[point] = xInitValA[point];
-            conditionAchievedA[point] = 0;
+            zImagAP[point] = imag;
+            zRealAP[point] = xInitValAP[point];
+            conditionAchievedAP[point] = 0;
         }
 
         // Iterate until every point threshold condition is met or iteration limit is reached
@@ -66,34 +80,36 @@ int *LineMandelCalculator::calculateMandelbrot() {
             int sum = 0;
 
             // Iterate over each complex number and
-            #pragma omp simd aligned(pdata, xInitValA, zImagA, zRealA, conditionAchievedA:128) reduction(+:sum)
+            #pragma omp simd aligned(xInitValAP, zImagAP, zRealAP, conditionAchievedAP:64) simdlen(64) reduction(+:sum) linear(current_iteration:1)
             for (int point = 0; point < width; point++) {
 
                 // Real and imaginary component on pow2
-                float r2 = zRealA[point] * zRealA[point];
-                float i2 = zImagA[point] * zImagA[point];
+                float r2 = zRealAP[point] * zRealAP[point];
+                float i2 = zImagAP[point] * zImagAP[point];
 
                 // Threshold condition cast to int for more optimal computation
                 auto conditionAchieved = static_cast<int>(r2 + i2 > 4.0f);
+
+                // Single cast to float (optimization purpose)
+                auto conditionAchievedF = static_cast<float>(conditionAchieved);
+
                 // Negation of condition (optimization purpose
                 auto conditionNotAchieved = static_cast<float>(1 - conditionAchieved);
 
                 // Counter of condition achievements by each point
                 // Subtracting it from max iteration reached we get on which iteration condition was firstly satisfied
-                conditionAchievedA[point] += conditionAchieved;
+                conditionAchievedAP[point] += conditionAchieved;
 
                 // Sum up condition achievements
                 sum += conditionAchieved;
 
-                // Single cast to float (optimization purpose)
-                auto conditionAchievedF = static_cast<float>(conditionAchieved);
 
                 // Calculate new imaginary and real component of current point
                 // We need to stop incrementing to not overflow -> ternary operator is slower than next lines
-                zImagA[point] = zImagA[point] * ((conditionNotAchieved * 2.0f * zRealA[point]) + conditionAchievedF) +
+                zImagAP[point] = zImagAP[point] * ((conditionNotAchieved * 2.0f * zRealAP[point]) + conditionAchievedF) +
                                 (conditionNotAchieved * imag);
-                zRealA[point] = (conditionAchievedF * zRealA[point]) +
-                                (conditionNotAchieved * (r2 - i2 + xInitValA[point]));
+                zRealAP[point] = (conditionAchievedF * zRealAP[point]) +
+                                (conditionNotAchieved * (r2 - i2 + xInitValAP[point]));
 
             }
             // Break limit loop if all points meets ending condition
@@ -103,12 +119,11 @@ int *LineMandelCalculator::calculateMandelbrot() {
                 int finished_iterations = current_iteration + 1;
 
                 // Save every point first condition met iteration
-                #pragma omp simd aligned(pdata, conditionAchievedA:128)
+                #pragma omp simd aligned(dataP, conditionAchievedAP:64) simdlen(64) linear(current_iteration:1)
                 for (int point = 0; point < width; point++) {
-                    pdata[row_shift + point] = finished_iterations - conditionAchievedA[point];
+                    dataP[row_shift + point] = finished_iterations - conditionAchievedAP[point];
                 }
                 break;
-
 
             }
         }
--- calculators/LineMandelCalculator.h
+++ calculators/LineMandelCalculator.h
@@ -15,43 +15,14 @@ public:
     int *calculateMandelbrot();
 
 private:
-    // @TODO add all internal parameters
     int *data;
     // Array of arranged Xs in range(x_start, x_start + width * dx)
-//    alignas(64) float* xInitValA;
-////
-//    // Arrays keeping imaginary and real values of each complex number on current row
-//    alignas(64) float* zRealA;
-//    alignas(64) float* zImagA;
-//
-//    // Array of counts of limit achievements of each complex number on current row
-//    alignas(64) int* conditionAchievedA;
-//
-//    // Cast floats to doubles
-//    float xStart;
-//    float yStart;
-//    float dxF;
-//    float dyF;
+    float* xInitValA;
 
-//    // Array of arranged Xs in range(x_start, x_start + width * dx)
-//    xInitValA = static_cast<float *>(aligned_alloc(64 * sizeof(float), width * sizeof(float)));
-//
-//    // Arrays keeping imaginary and real values of each complex number on current row
-//    zRealA = static_cast<float *>(aligned_alloc(64 * sizeof(float), width * sizeof(float)));
-//    zImagA = static_cast<float *>(aligned_alloc(64 * sizeof(float), width * sizeof(float)));
-//
-//    // Array of counts of limit achievements of each complex number on current row
-//    conditionAchievedA = static_cast<int *>(aligned_alloc(64 * sizeof(int), width * sizeof(int)));
-//
-//    // Cast floats to doubles
-//    xStart = static_cast<float>(x_start);
-//    yStart = static_cast<float>(y_start);
-//    dxF = static_cast<float>(dx);
-//    dyF = static_cast<float>(dy);
-//
-//    // Arrange Xs
-//#pragma omp simd
-//    for (size_t point = 0; point < width; point++) {
-//        xInitValA[point] = xStart + static_cast<float>(point) * dxF;
-//    }
+    // Arrays keeping imaginary and real values of each complex number on current row
+    float* zRealA;
+    float* zImagA;
+
+    // Array of counts of limit achievements of each complex number on current row
+    int* conditionAchievedA;
 };
